ea20485715815c0c7ef25d6e613f77da25516e96
Andjelina <astojanovic1900@gmail.com>
2022-05-13 21:04:00 +0200

    Add

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import FileSaver from 'file-saver';
 
 function App() {
+  const [input, setInput] = useState('');
   const [templates, setTemplates] = useState([]);
   const [drop, setDrop] = useState('');
   const [url, setUrl] = useState('');
@@ -15,6 +16,7 @@ function App() {
   const saveFile = () => {
     FileSaver.saveAs(image, 'meme.png');
   };
+
   useEffect(() => {
     (async () => {
       try {
@@ -28,76 +30,116 @@ function App() {
       console.log(error);
     });
   }, []);
-
   return (
-    <div>
-      <select
-        className="input"
-        onChange={(event) => {
-          setDrop(event.currentTarget.value);
-        }}
-      >
+    <div className="container">
+      <label>
+        <select
+          className="input"
+          // value={setTemplates}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter') {
+              event.preventDefault();
+              setImage(
+                `https://api.memegen.link/images/${drop}/${toptext}/${bottomtext}.png`,
+              );
+            }
+          }}
+          onChange={(event) => {
+            setDrop(event.currentTarget.value);
+          }}
+        >
+          <option>Meme Templates</option>
+          {templates.map((template) => (
+            <option key={template.id} value={template.id}>
+              {template.name}
+            </option>
+          ))}
+        </select>
+      </label>
+      <div className="ok">
+        <img
+          data-test-id="meme-image"
+          style={{
+            width: '100%',
+            height: '300px',
+            margin: '30px 0 30px 0',
+            borderRadius: '20px',
+            boxShadow: '2px solid black',
+          }}
+          src={image}
+          alt=""
+        />
+        <form>
+          <label className="inp">
+            Enter top text here:
+            <br />
+            <input
+              className="inp"
+              value={toptext}
+              placeholder="top text"
+              onChange={(event) => {
+                setToptext(event.currentTarget.value);
+              }}
+            />
+          </label>
+          <br />
+          <br />
+          <label className="inp">
+            Enter top text here:
+            <br />
+            <input
+              className="inp"
+              value={bottomtext}
+              placeholder="bottom text"
+              onChange={(event) => {
+                setBottomtext(event.currentTarget.value);
+              }}
+            />
+          </label>
+        </form>
+      </div>
+      <div className="button">
+        <button
+          className="jump"
+          onClick={() => {
+            setImage(
+              `https://api.memegen.link/images/${drop}/${toptext}/${bottomtext}.png`,
+            );
+          }}
+        >
+          Generate
+        </button>
+        <button
+          className="btn"
+          onClick={() => {
+            console.log(image);
+            setImage(image);
+            saveFile();
+          }}
+        >
+          Download
+        </button>
+        <button
+          className="btn"
+          onClick={() => setToptext('')(setBottomtext(''))}
+        >
+          Clear
+        </button>
+      </div>
+      {/* <label>
+        <input
+          className="input"
+          value={input}
+          onChange={(event) => {
+            setInput(event.target.value);
+          }}
+        />
         {templates.map((template) => (
           <option key={template.id} value={template.id}>
             {template.name}
           </option>
         ))}
-        fffff
-      </select>
-      <img
-        style={{
-          width: '500px',
-          height: '500px',
-          border: '1px solid black',
-          margin: '30px 0 30px 0',
-        }}
-        src={image}
-        alt=""
-      />
-      <form>
-        <label>
-          Enter top text here:
-          <br />
-          <input
-            value={toptext}
-            onChange={(event) => {
-              setToptext(event.currentTarget.value);
-            }}
-          />
-        </label>
-        <br />
-        <br />
-        <label>
-          Enter top text here:
-          <br />
-          <input
-            value={bottomtext}
-            onChange={(event) => {
-              setBottomtext(event.currentTarget.value);
-            }}
-          />
-        </label>
-      </form>
-      <button
-        className="btn"
-        onClick={() => {
-          setImage(
-            `https://api.memegen.link/images/${drop}/${toptext}/${bottomtext}.png`,
-          );
-        }}
-      >
-        Generate
-      </button>
-
-      <button
-        onClick={() => {
-          console.log(image);
-          setImage(image);
-          saveFile();
-        }}
-      >
-        Download
-      </button>
+      </label> */}
     </div>
   );
 }
